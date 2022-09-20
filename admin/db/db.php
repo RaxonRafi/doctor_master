cd58013292e02fd2617b750d1cd33f10ed9cad9a
Muhammad Rafi <mr1633470rafi@gmail.com>
2022-09-20 20:53:16 +0600

    visit notes update page

--- admin/db/db.php
+++ admin/db/db.php
@@ -2,7 +2,7 @@
 $hostname = "localhost";
 $username = "root";
 $password = "";
-$db_name = "doctor_master";
+$db_name = "doctor_master1";
 
 $db_connect = mysqli_connect($hostname,$username,$password,$db_name);
 
--- admin/visitnotes_update.php
+++ admin/visitnotes_update.php
@@ -1,38 +1,27 @@
-<?php
+ <?php
 
-$patient_details = true;
-require_once "inc/header.php";
-require_once "db/db.php";
-$url = "https://devapi.oxyjon.com/api/doctors/masterdata";
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$result = curl_exec($ch);
-curl_close($ch);
-$result_after_decode = json_decode($result);
-$conditions = $result_after_decode->data->HealthCondition;
-$procedures = $result_after_decode->data->Procedure;
+ $patient_details = true;
+ require_once "inc/header.php";
+ require_once "db/db.php";
+// $url = "https://devapi.oxyjon.com/api/doctors/masterdata";
+// $ch = curl_init();
+// curl_setopt($ch, CURLOPT_URL, $url);
+// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+// $result = curl_exec($ch);
+// curl_close($ch);
+// $result_after_decode = json_decode($result);
+// $conditions = $result_after_decode->data->HealthCondition;
+// $procedures = $result_after_decode->data->Procedure;
 
 
 
-?>
+?> 
 <!-- Breadcrumb -->
 <!-- Page Title -->
 <div class="container-fluid mt-0">
     <div class="row breadcrumb-bar">
         <div class="col-md-6">
-            <h3 class="block-title">Visit Notes</h3>
-        </div>
-        <div class="col-md-6">
-            <ol class="breadcrumb">
-                <li class="breadcrumb-item">
-                    <a href="index.html">
-                        <span class="ti-home"></span>
-                    </a>
-                </li>
-                <li class="breadcrumb-item">Patients</li>
-                <li class="breadcrumb-item active">Visit Notes</li>
-            </ol>
+            <h3 class="block-title">Visit Notes Update</h3>
         </div>
     </div>
 </div>
@@ -48,8 +37,7 @@ $procedures = $result_after_decode->data->Procedure;
         <!-- Widget Item -->
         <div class="col-md-12">
             <div class="widget-area-2 proclinic-box-shadow">
-            <a class="btn btn-success" href="medicines.php?id=<?php echo $_SESSION['patient_id']?>" type="button" >Go to Medicines</a>
-                <!-- <h3 class="widget-title">Visit Notes</h3> -->
+                <!-- <h3 class="widget-title">Visit Notes Update</h3> -->
                 <form method="POST" enctype="multipart/form-data" action="visitNotes_post.php">
 
                     <input name="doc_id" value="<?php echo $_SESSION['doc_id'] ?>" type="hidden">
@@ -277,7 +265,7 @@ $procedures = $result_after_decode->data->Procedure;
                             </label>
                         </div>
 
-                        <button target="_blank" name="submit" type="submit" class="btn btn-primary mt-3">Submit</button>
+                        <button target="_blank" name="submit" type="submit" class="btn btn-primary mt-3">Update</button>
                         </from>
 
 
@@ -290,311 +278,6 @@ $procedures = $result_after_decode->data->Procedure;
     </div>
 </div>
 <!-- /Main Content -->
-<?php
-$id = $_SESSION['patient_id'];
-$select_query = "SELECT * FROM visit_notes WHERE patient_id = $id";
-$data = mysqli_fetch_assoc( mysqli_query($db_connect,$select_query));
-
-?>
-
-<div class="container-fluid">
-    <div class="row">
-        <!-- Widget Item -->
-        <div class="col-md-12">
-            <div class="widget-area-2 proclinic-box-shadow">
-                <h3 class="widget-title">Visit Notes</h3>
-
-                <a type="button" target="_black" class="btn btn-info float-right" href="pdf/index.php?id=<?php echo $_SESSION['patient_id']  ?>">Preview Prescription</a>
-                <a type="button"class="btn btn-success mr-2 float-right" href="visitnotes_update.php?id=<?php echo $_SESSION['patient_id'] ?>"> <span class="ti-pencil-alt" ></span> Edit</a>
-                <div class="table-responsive">
-                    <table class="table table-bordered">
-                        <tbody>
-                        <tr>
-                                <td><strong>Blood Test Report</strong></td>
-                                <?php
-                               if(isset($data['blood_test_report'])){
-                                ?>
-                                <td><a href="../admin/reports/<?php echo $data['blood_test_report']?>" download> Download Blood Test Report </a></td>
-                                <?php
-                               };
-                                ?>
-                                
-                            </tr>
-                            <tr>
-                                <td><strong>Report Name</strong></td>
-                                <?php
-                               if(isset($data['report_name'])){
-                                ?>
-                                <td><?php echo $data['report_name'] ?></td>
-                                <?php
-                               };
-                                ?>
-                                
-                            </tr>
-                            <tr>
-                                <td><strong>Examination</strong></td>
-                                <?php
-                               if(isset($data['examination'])){
-                                ?>
-                                <td><?php echo $data['examination'] ?></td>
-                                <?php
-                               };
-                                ?>
-                            </tr>
-                            <!-- <tr>
-                                <td><strong>Health Parameters</strong> </td>
-
-                            </tr> -->
-                            <tr>
-                                <td><strong>Blood Pressure</strong></td>
-                                <?php
-                               if(isset($data['blood_pressure'])){
-                                ?>
-                                <td><?php echo $data['blood_pressure'] ?></td>
-                                <?php
-                               };
-                                ?>
-                            </tr>
-                            <tr>
-                                <td><strong> Pulse</strong></td>
-                                <?php
-                               if(isset($data['blood_pulse'])){
-                                ?>
-                                <td><?php echo $data['blood_pulse'] ?></td>
-                                <?php
-                               };
-                                ?>
-                            </tr>
-                            <tr>
-                                <td><strong>SPO2 </strong></td>
-                                <?php
-                               if(isset($data['spo2'])){
-                                ?>
-                                <td><?php echo $data['spo2'].""."%" ?></td>
-                                <?php
-                               };
-                                ?>
-                            </tr>
-                            <tr>
-                                <td><strong> Fasting Blood Sugar</strong></td>
-                                <?php
-                               if(isset($data['fasting_blood_sugar'])){
-                                ?>
-                                <td><?php echo $data['fasting_blood_sugar'] ?></td>
-                                <?php
-                               };
-                                ?>
-                            </tr>
-                            <tr>
-                                <td><strong> Random Blood Sugar</strong></td>
-                                <?php
-                               if(isset($data['random_blood_sugar'])){
-                                ?>
-                                <td><?php echo $data['random_blood_sugar'] ?></td>
-                                <?php
-                               };
-                                ?>
-                            </tr>
-                            <tr>
-                                <td><strong> HbA1c </strong></td>
-                                <?php
-                               if(isset($data['hbaic'])){
-                                ?>
-                                <td><?php echo $data['hbaic'] ?></td>
-                                <?php
-                               };
-                                ?>
-                            </tr>
-                            <tr>
-                                <td><strong> Creatinine</strong></td>
-                                <?php
-                               if(isset($data['creatinine'])){
-                                ?>
-                                <td><?php echo $data['creatinine'] ?></td>
-                                <?php
-                               };
-                                ?>
-                            </tr>
-                            <tr>
-                                <td><strong> Urine MACR</strong></td>
-                                <?php
-                               if(isset($data['urine_macr'])){
-                                ?>
-                                <td><?php echo $data['urine_macr'] ?></td>
-                                <?php
-                               };
-                                ?>
-                            </tr>
-                            <tr>
-                                <td><strong> BUN</strong></td>
-                                <?php
-                               if(isset($data['bun'])){
-                                ?>
-                                <td><?php echo $data['bun'] ?></td>
-                                <?php
-                               };
-                                ?>
-                            </tr>
-                            <tr>
-                                <td><strong> vit_d3</strong></td>
-                                <?php
-                               if(isset($data['vit_d3'])){
-                                ?>
-                                <td><?php echo $data['vit_d3'] ?></td>
-                                <?php
-                               };
-                                ?>
-                            </tr>
-                            <tr>
-                                <td><strong> vit_b12</strong></td>
-                                <?php
-                               if(isset($data['vit_b12'])){
-                                ?>
-                                <td><?php echo $data['vit_b12'] ?></td>
-                                <?php
-                               };
-                                ?>
-                            </tr>
-                            <tr>
-                                <td><strong> uric_acid</strong></td>
-                                <?php
-                               if(isset($data['uric_acid'])){
-                                ?>
-                                <td><?php echo $data['uric_acid'] ?></td>
-                                <?php
-                               };
-                                ?>
-                            </tr>
-                            <tr>
-                                <td><strong> sgot</strong></td>
-                                <?php
-                               if(isset($data['sgot'])){
-                                ?>
-                                <td><?php echo $data['sgot'] ?></td>
-                                <?php
-                               };
-                                ?>>
-                            </tr>
-                            <tr>
-                                <td><strong> sgpt</strong></td>
-                                <?php
-                               if(isset($data['sgpt'])){
-                                ?>
-                                <td><?php echo $data['sgpt'] ?></td>
-                                <?php
-                               };
-                                ?>
-                            </tr>
-                            <tr>
-                                <td><strong>Father Condition</strong></td>
-                                <?php
-                               if(isset($data['father_condition'])){
-                                ?>
-                                <td><?php echo $data['father_condition'] ?></td>
-                                <?php
-                               };
-                                ?>
-                            </tr>
-                            <tr>
-                            <td><strong>Mother Condition </strong></td>
-                                <?php
-                               if(isset($data['mother_condition'])){
-                                ?>
-                                <td><?php echo $data['mother_condition'] ?></td>
-                                <?php
-                               };
-                                ?>
-                            </tr>
-                            <tr>
-                            <td><strong>sister Condition</strong></td>
-                                <?php
-                               if(isset($data['sister_condition'])){
-                                ?>
-                                <td><?php echo $data['sister_condition'] ?></td>
-                                <?php
-                               };
-                                ?>
-                            </tr>
-                            <tr>
-                               <td><strong>Brother Condition</strong></td>
-                                <?php
-                               if(isset($data['brother_condition'])){
-                                ?>
-                                <td><?php echo $data['brother_condition'] ?></td>
-                                <?php
-                               };
-                                ?>
-                            </tr>
-                            <tr>
-                                <td><strong>patient Conditions </strong></td>
-                                <?php
-                               if(isset($data['provisional_diagnosis'])){
-                                ?>
-                                <td><?php echo $data['provisional_diagnosis'] ?></td>
-                                <?php
-                               };
-                                ?>
-                            </tr>
-                            <tr>
-                                <td><strong>Investigation </strong></td>
-                                <?php
-                               if(isset($data['investigation'])){
-                                ?>
-                                <td><?php echo $data['investigation'] ?></td>
-                                <?php
-                               };
-                                ?>
-                            </tr>
-                            <tr>
-                            <tr>
-                                <td><strong>Referral</strong></td>
-                                <?php
-                               if(isset($data['referral'])){
-                                ?>
-                                <td><?php echo $data['referral'] ?></td>
-                                <?php
-                               };
-                                ?>
-                            </tr>
-                            <tr>
-                                <td><strong>Advise for Procedure </strong></td>
-                                <?php
-                               if(isset($data['advise_for_procedure'])){
-                                ?>
-                                <td><?php echo $data['advise_for_procedure'] ?></td>
-                                <?php
-                               };
-                                ?>
-                            </tr>
-                            <tr>
-                                <td><strong>Prescription </strong></td>
-                                <?php
-                               if(isset($data['prescription'])){
-                                ?>
-                                <td><?php echo $data['prescription'] ?></td>
-                                <?php
-                               };
-                                ?>
-                            </tr>
-                            <tr>
-                                <td><strong>Repeat visit date</strong></td>
-                                <?php
-                               if(isset($data['repeat_visit'])){
-                                ?>
-                                <td><?php echo $data['repeat_visit_date']." ".$data['repeat_visit'] ?></td>
-                                <?php
-                               };
-                                ?>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-
-            </div>
-        </div>
-        <!-- /Widget Item -->
-    </div>
-</div>
 <?php
 require_once "inc/footer.php"
 ?>
